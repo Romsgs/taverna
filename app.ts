0e0f2008b5ada7780782c4653c2c9b1ec3dfcd36
Romsgs <rsalobrena@gmail.com>
2022-05-17 15:27:31 -0300

    :recycling: change some things

--- app.ts
+++ app.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import mainRouter from './src/routes/mainRouter';
-
+import config from 'config'
 const app = express()
 
 app.use(express.json())
 app.use(express.static('public'))
 app.set("view engine", "ejs");
 app.use('/shows',mainRouter)
+app.use('/',mainRouter)
 
-app.listen(3000, ()=>{console.log('listening on 3000')})
+app.listen(config.get('PORT'), ()=>{console.log(`listening on ${config.get('HOST')}${config.get('PORT')}`)})
---
+++ config/default.ts
@@ -0,0 +1,4 @@
+export default {
+  PORT: 3000,
+  HOST: "https://heroku:"
+}
